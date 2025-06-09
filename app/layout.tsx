9efbcb10402a4960a72eb8e6ed5d474993497107
Zywl <lifeerwz@gmail.com>
2025-06-09 18:43:08 -0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,26 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type React from "react"
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
-  title: 'ASCII',
-  description: 'ASCII',
+  title: "ASCII",
+  description: "Convert images to ASCII",
+  icons: {
+    icon: "/favicon.png",
+  },
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body>{children}</body>
-    </html>
+      </html>
   )
 }
